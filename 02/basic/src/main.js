bd7df4b2f20d8ff021ce4b2bb79d81a1d97bd81a
donghoonlee94 <94donghoon.lee@gmail.com>
2022-06-21 22:24:33 +0900

    gsap 추가

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -5,7 +5,7 @@
 // import example from './ex05';
 // import example from './ex06';
 // import example from './ex06_02';
-import example from './ex07';
-// import example from './ex08';
+// import example from './ex07';
+import example from './ex08';
 
 example();
