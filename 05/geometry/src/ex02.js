556cd10da2af2df641ac8f7da6ea0cabc2049a9a
donghoonlee94 <94donghoon.lee@gmail.com>
2022-06-22 21:54:24 +0900

    add: geometry float32array 조작

--- 05/geometry/src/ex02.js
+++ 05/geometry/src/ex02.js
@@ -18,7 +18,7 @@ export default function example() {
 
   // Camera
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 4;
+  camera.position.z = 10;
   scene.add(camera);
 
   // Light
@@ -32,15 +32,16 @@ export default function example() {
 
   const controls = new OrbitControls(camera, renderer.domElement);
 
-  // Mesh
-  const geometry = new THREE.BoxGeometry(1, 1, 1, 32, 32, 32);
+  const geometry = new THREE.SphereGeometry(5, 64, 64);
   const material = new THREE.MeshStandardMaterial({
-    color: 'hotpink',
+    color: 'orangered',
     side: THREE.DoubleSide,
-    wireframe: true,
+    flatShading: true,
   });
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+
+  console.log(geometry.attributes.position.array);
 
   // 그리기
   const clock = new THREE.Clock();
